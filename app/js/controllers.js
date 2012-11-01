3ede486f94f05c82fa9961877c0955b054ea600a
Stanislaw Smetanin <stanislaw@smetanin-online.name>
2012-11-01 15:37:55 +0400

    more demo data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,18 +6,62 @@ var locations = [
 	]},
 	{"id":2, "title":"МПx", "address":"Молодежная", "devices":[
 		{ "id":2, "title":"монитор", "num":"123556e" },
-		{ "id":3, "title":"системник", "num":"123123f" }
+		{ "id":3, "title":"системник", "num":"123123f" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":2, "title":"монитор", "num":"123556e" }
 	]}
 ];
 
 /* Controllers */
 function LocationsListCtrl($scope)
 {
+	var self = this;
+	var selectedIdx = 0;
+
+	self.toggleActive = function(idx) {
+		console.log('toggle');
+
+		if (idx === self.selectedIdx) {
+			self.selectedIdx = 0;
+		} else {
+			self.selectedIdx = idx;
+		}
+	}
+
+	self.getClass = function(idx) {
+		console.log('get class');
+		var cls = '';
+
+		if (idx === self.selectedIdx) {
+			cls = 'active';
+		} 
+
+		return cls;
+	}
+
 	$scope.locations = locations;
 }
 
 function DevicesListCtrl($scope, $routeParams)
-{  
+{
 	for (var idx in locations) {
 		var location = locations[idx];
 
