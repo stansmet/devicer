2e1c1945409577e4a1ec605475ef5e57256d4f7b
Stanislaw Smetanin <stanislaw@smetanin-online.name>
2012-11-01 14:58:08 +0400

    frontend for list of locations & list of devices

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,19 @@
 'use strict';
 
+angular.module('App', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      //when('/', {controller: LocationsListCtrl}).
+      when('/devices/:locationId', {templateUrl: 'partials/devicesList.html', controller: DevicesListCtrl})
+      //otherwise({redirectTo: '/'});
+}]);
 
 // Declare app level module which depends on filters, and services
+/*
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {controller: DeviceListCtrl});
     //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
+  */
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,28 @@
 'use strict';
 
+var locations = [
+	{"id":1, "title":"БВИ", "address":"Аврора парк", "devices":[
+		{ "id":1, "title":"роутер", "num":"123123d" }
+	]},
+	{"id":2, "title":"МПx", "address":"Молодежная", "devices":[
+		{ "id":2, "title":"монитор", "num":"123556e" },
+		{ "id":3, "title":"системник", "num":"123123f" }
+	]}
+];
+
 /* Controllers */
-
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
-
-function DeviceListCtrl($scope)
+function LocationsListCtrl($scope)
 {
-	$scope.locations = [
-		{"id":1, "title":"БВИ", "address":"Аврора парк"},
-		{"id":2, "title":"МПx", "address":"Молодежная"}
-	];
+	$scope.locations = locations;
+}
+
+function DevicesListCtrl($scope, $routeParams)
+{  
+	for (var idx in locations) {
+		var location = locations[idx];
+
+		if (location.id == $routeParams.locationId) {
+			$scope.devices = location.devices;
+		}
+	}
 }
